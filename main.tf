80622f94d85af4d50ba0952f48aa1c1ed391b2dd
Sahil Diwan <sahildwn@terpmail.umd.edu>
2020-02-09 11:45:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_vpc" "default" {
 resource "aws_subnet" "public-subnet" {
   vpc_id = "${aws_vpc.default.id}"
   cidr_block = "${var.public_subnet_cidr}"
-  availability_zone = "ap-southeast-1"
+  availability_zone = "us-east-1"
 
   tags {
     Name = "Web Public Subnet"
